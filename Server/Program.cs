3539aca94fd886d43ffa8bcd425c8ffac73280f2
Ramon Ramos <ramonramos.silva19@gmail.com>
2023-01-02 12:16:40 -0300

    feat: client js

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,25 +2,27 @@ using Microsoft.AspNetCore.SignalR;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
+builder.Services.AddCors();
 
 var app = builder.Build();
-app.MapHub<MyHub>("/chat");
 
+app.UseCors(x => x
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .SetIsOriginAllowed(origin => true) // allow any origin
+                    .WithOrigins("http://127.0.0.1:5500") // Allow only this origin can also have multiple origins separated with comma
+                    .AllowCredentials()); // allow credentials
+
+app.MapHub<ChatHub>("/chat");
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
 
-class MyHub : Hub
+public class ChatHub : Hub
 {
-    public async IAsyncEnumerable<DateTime> Streaming(
-        CancellationToken cancellationToken
-    )
+    public async Task SendMessage(string user, string message)
     {
-        while (true)
-        {    
-            yield return DateTime.Now;
-            await Task.Delay(1000, cancellationToken);
-        }
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
     }
 }
